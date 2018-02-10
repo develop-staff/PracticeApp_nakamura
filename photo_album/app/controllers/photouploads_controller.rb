0cdb1a116257f22bff6e10f7c0f91a0741e9f9a5
nakamura <meisei.nakamura@cacco.co.jp>
2018-02-11 04:58:17 +0900

    アップロードされるファイルの拡張子の制限(.jpg, .png)の実装

--- photo_album/app/controllers/photouploads_controller.rb
+++ photo_album/app/controllers/photouploads_controller.rb
@@ -1,10 +1,10 @@
 class PhotouploadsController < ApplicationController
   def index
     @img_paths=[]
-    jpgs=Dir.glob(Rails.root.join('public','*jpg'))
+    extes=Dir.glob(Rails.root.join('public','*{jpg,jpeg,png}'))
 
-    jpgs.each do |jpg|
-      @img_paths.push('/'+File.basename(jpg))
+    extes.each do |exte|
+      @img_paths.push('/'+File.basename(exte))
     end
 
     @img_paths.shuffle!
@@ -12,19 +12,27 @@ class PhotouploadsController < ApplicationController
 
 
   def create
-    uploaded_photo=photoupload_param[:file]
-    output_path=Rails.root.join('public',uploaded_photo.original_filename)
+      uploaded_photo=photoupload_param[:file]
+      name=uploaded_photo.original_filename
 
-    File.open(output_path,'w+b')do|fp|
-      fp.write uploaded_photo.read
-    end
+      check_file=(File.extname(name)=='.jpg'||File.extname(name)=='.jpeg'||File.extname(name)=='.png')
+
+      if check_file
+        if (uploaded_photo.size < 1.megabytes)
+          output_path=Rails.root.join('public',name)
+
+          File.open(output_path,'w+b')do|fp|
+            fp.write uploaded_photo.read
+          end
+        end
+      end
 
     redirect_to action: 'index'
   end
+
 
   private
   def photoupload_param
     params.require(:photoupload).permit(:file)
   end
 end
-
