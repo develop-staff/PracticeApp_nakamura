e13384332c7e22750f56137ef4eebdd1a70d1c8f
nakamura <meisei.nakamura@cacco.co.jp>
2018-02-23 15:15:11 +0900

    アップロードされた画像が画面に一覧表示される機能を実装

--- demo/src/main/java/com/example/demo/SampleController.java
+++ demo/src/main/java/com/example/demo/SampleController.java
@@ -4,23 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.*;
+import java.io.FileNotFoundException;
 
+import java.lang.reflect.Array;
 import java.nio.file.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 
 @Controller
 public class SampleController {
 
 
-    @RequestMapping(path = "/upload", method = RequestMethod.GET)
-    String uploadview(Model model) {
-        return "/upload";
-    }
+//    @RequestMapping(path = "/upload", method = RequestMethod.GET)
+//    String uploadview(Model model) {
+//        return "/upload";
+//    }
 
     @RequestMapping(path = "/upload", method = RequestMethod.POST)
     String upload(Model model, UploadForm uploadForm) {
@@ -28,7 +32,8 @@ public class SampleController {
             return "/upload";
         }
 
-        Path path = Paths.get("/Users/meisei/IdeaProjects/demo/src/main/resources/static");
+        //Path path = Paths.get("/Users/nakamura/PracticeApp_nakamura/demo/image");  TODO
+        Path path = Paths.get("/Users/nakamura/PracticeApp_nakamura/demo/src/main/resources/static");
         if (!Files.exists(path)) {
             try {
                 Files.createDirectory(path);
@@ -41,7 +46,8 @@ public class SampleController {
 
         String filename=uploadForm.getFile().getOriginalFilename();
         Path uploadfile = Paths
-                .get("/Users/meisei/IdeaProjects/demo/src/main/resources/static/" + filename);
+                .get("/Users/nakamura/PracticeApp_nakamura/demo/src/main/resources/static/"+filename);
+                //.get("/Users/nakamura/PracticeApp_nakamura/demo/image/" + filename);  TODO
 
         try (OutputStream os = Files.newOutputStream(uploadfile, StandardOpenOption.CREATE)) {
             byte[] bytes = uploadForm.getFile().getBytes();
@@ -55,17 +61,26 @@ public class SampleController {
         return "redirect:/upload";
     }
 
-    //たぶん呼び出されていない。
-    //value, methodを指定すると、なぜかエラーが生じる。
-    //@RequestMapping(value = "/upload",method = RequestMethod.GET)
-    @RequestMapping("/upload")
+
+    @RequestMapping(path = "/upload",method = RequestMethod.GET)
     ModelAndView show(ModelAndView mav){
         mav.setViewName("upload");
-        ArrayList<File>files=new ArrayList<File>();
-        files.add(new File("/*.jpg"));
 
-        String imgs=files.toString();
-        mav.addObject("files",imgs);
+        ArrayList<String>img=new ArrayList<>();
+
+        //File dir=new File("/Users/nakamura/PracticeApp_nakamura/demo/image");  TODO
+        File dir=new File("/Users/nakamura/PracticeApp_nakamura/demo/src/main/resources/static");
+        File[]files=dir.listFiles();
+
+        for(int i=0;i<files.length;i++){
+            //img.add(new File("/Users/nakamura/PracticeApp_nakamura/demo/image/"+files[i]).getName());  TODO
+            img.add(new File("/Users/nakamura/PracticeApp_nakamura/demo/src/main/resources/static/"+files[i]).getName());
+        }
+
+        Collections.shuffle(img);
+        ArrayList<String>result=img;
+
+        mav.addObject("files",img);
         return mav;
     }
 }
