2ef4512bace83eb645ae58643afb1eb29b903d53
nakamura <meisei.nakamura@cacco.co.jp>
2018-03-05 02:52:58 +0900

    インターフェイスの試実装・動作確認

--- songs_album/src/main/java/com/example/songs_album/ShuffleEngine.java
+++ songs_album/src/main/java/com/example/songs_album/ShuffleEngine.java
@@ -3,13 +3,15 @@ package com.example.songs_album;
 //曲の再生が１巡する間に、なるべく同じ曲がかかりにくくする工夫をする
 //最初に１巡分の曲順を決定する場合、２巡目になったら１巡目と違う順番になるようにする
 
+import java.util.List;
+
 //「シャッフル」とは、たとえば５０個くらいの曲から５つをランダムに選んでリストに表示し、
 //再生していく(現在選択しているファイルをブラウザに表示していく)て感じ。
 public interface ShuffleEngine {
     public final Integer PEEKMAX=5;
 
     //シャッフル対象の曲(Song)の配列をインスタンスに設定する
-    void setSongs(Song[]songs);
+    void setSongs(Song[] songs);
 
     //次に再生する曲(Song)を返す。次に返す曲が更新される。
     Song getNextSong();
--- songs_album/src/main/java/com/example/songs_album/SongController.java
+++ songs_album/src/main/java/com/example/songs_album/SongController.java
@@ -3,7 +3,14 @@ package com.example.songs_album;
 import com.example.songs_album.repositories.SongRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -15,21 +22,70 @@ public class SongController implements ShuffleEngine{
     @Autowired
     SongRepository repository;
 
-    ArrayList<Song>songsList;
-    //ArrayList<Song>songsList=new ArrayList<>(repository.findAll());
+    //全体の
+    ArrayList<Song>songsList=new ArrayList<>();
 
+    String storePath="/Users/meisei/Documents/abc_app/songs_album/target/classes/static";
+
+    //表示されている５つの曲のうち、何番目か
     int playCount=0;
 
+    //表示されている５つの曲の配列
+    Song[]songs=new Song[5];
 
-    //TODO
-    //シャッフル対象の曲(Song)の配列をインスタンス(何の？)に設定
-    //再生する５曲というよりは、対象となる曲全体のことを指すはず。
-    //peakQueueとの兼ね合いはどうするのか
-    public void setSongs(Song[]songs){  //引数には、「シャッフル対象の曲」(たぶんrepository.findAll())
-        Collections.addAll(this.songsList,songs);
+
+    @PostConstruct
+    //ダミーデータ(10個)の作成
+    public void init(){
+
+        File file[]=new File[10];
+        Song song[]=new Song[10];
+        for(int i=0;i<10;i++){
+            file[i]=new File(storePath+"/song_"+(i)+".mp3");
+            song[i]=new Song();
+            song[i].setFile_name(file[i].getName());
+            song[i].setFile_path("song_"+(i)+".mp3");
+
+            repository.saveAndFlush(song[i]);
+        }
     }
 
     //TODO
+    @RequestMapping(path = "/",method = RequestMethod.GET)
+    ModelAndView index(ModelAndView mav){
+        mav.setViewName("index");
+        setSongs(repository.findAll().toArray(new Song[repository.findAll().size()]));
+        mav.addObject("files",this.songsList);
+        mav.addObject("songs",peekQueue());
+
+        return mav;
+    }
+
+
+    @RequestMapping(path = "/shuffle",method = RequestMethod.GET)
+    String shuffleSongs(Model model){
+        model.addAttribute("songs",peekQueue());
+        return "redirect:/";
+    }
+
+//    @RequestMapping(path = "/play",method = RequestMethod.GET)
+//    ModelAndView playSong(ModelAndView mav){
+//
+//    }
+
+
+
+    //シャッフル対象の曲(Song)の配列をインスタンス(何の？)に設定
+    //再生する５曲というよりは、対象となる曲全体のことを指すはず。
+    //peakQueueとの兼ね合いはどうするのか
+    //↓ここもうちょっと綺麗にかける
+    //呼び出し側で、setSongs(repository.findAll().toArray(new Song[repository.findAll().size()]));
+    public void setSongs(Song[] songs){  //引数には、「シャッフル対象の曲」(たぶんrepository.findAll())
+        this.songsList.clear();
+        Collections.addAll(this.songsList,songs);  //これ単体だと、呼び出されるたびにsongsListに(前の情報を残したまま)「追加」され続けてしまう。
+    }
+
+
     //次に再生する曲(Song)を返す。次に返す曲が更新される。再生ボタン押されたとき(playSong()の中で？)呼ばれる。
     public Song getNextSong(){
 
@@ -39,7 +95,7 @@ public class SongController implements ShuffleEngine{
         return nextSong;
     }
 
-    //TODO
+
     //次に再生する予定の曲を先読み(PEEKMAXを上限)して配列として返す。
     public Song[]peekQueue(){
 
