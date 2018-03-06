3553366185b3b879c9de7fd5a97d57619f5b3748
nakamura <meisei.nakamura@cacco.co.jp>
2018-03-06 13:43:17 +0900

    「シャッフルボタン」を押さないで「再生」を押した際のエラーを解消

--- songs_album/src/main/java/com/example/songs_album/SongController.java
+++ songs_album/src/main/java/com/example/songs_album/SongController.java
@@ -31,8 +31,11 @@ public class SongController implements ShuffleEngine{
     int selectedSongIndex=0;
 
     //表示されている５つの曲の配列
-    Song[]songs=new Song[5];
+    //Song[]songs=new Song[5];
     Song[] presentSongsList=new Song[5];
+    Song[] nextSongsList=new Song[5];
+
+    String _selectedSongName;
 
     @PostConstruct
     //ダミーデータ(10個)の作成
@@ -45,23 +48,33 @@ public class SongController implements ShuffleEngine{
             song[i]=new Song();
             song[i].setFile_name(file[i].getName());
             song[i].setFile_path("song_"+(i)+".mp3");
-            presentSongsList=peekQueue();
             repository.saveAndFlush(song[i]);
         }
+        //TODO
+        //setSongs書き換えるので、ここも変えねばならない
+        setSongs(repository.findAll().toArray(new Song[repository.findAll().size()]));
+        //setSongs(peekQueue());
 
-        songs=peekQueue();
+        presentSongsList=peekQueue();
     }
+
+
 
     //TODO
     @RequestMapping(path = "/",method = RequestMethod.GET)
     ModelAndView index(ModelAndView mav){
         mav.setViewName("index");
         setSongs(repository.findAll().toArray(new Song[repository.findAll().size()]));
-        //mav.addObject("files",this.songsList);
-        //mav.addObject("songs",peekQueue());
+        //setSongs(peekQueue();
+
+        _selectedSongName=presentSongsList[selectedSongIndex].getFile_name();
+        mav.addObject("selectedSong",_selectedSongName);
+
         mav.addObject("songs",presentSongsList);
+
         return mav;
     }
+
 
 
     //たぶんシャッフルされて並んでいる５つの曲を表示するという処理も、index()に書いた方がよさそう。
@@ -77,36 +90,46 @@ public class SongController implements ShuffleEngine{
     //音楽再生とか、次の曲再生の時にランダムに変わるのは、redirect:/でそこからまたindexが呼ばれるからだろう。
     //再生するのを表示する処理は、index()に書いた方がよいかも
     //ここはあくまで、曲の指定するのみがよい？
+    //TODO
+    //現在、「シャッフルボタン」を押さないで「再生」ボタンを押すとエラーが生じる
+    //「シャッフルボタン」で何を初期化しているか注意！
+    //TODO
+    //初めの段階で、現在の曲がnullなのは、ボタンを押さないとselectedSongに値が入らない書き方だから。
     @RequestMapping(path = "/play",method = RequestMethod.GET)
     String playSong(RedirectAttributes attributes){
-        String _selectedSongName=presentSongsList[selectedSongIndex].getFile_name();
+        _selectedSongName=presentSongsList[selectedSongIndex].getFile_name();
         attributes.addFlashAttribute("selectedSong",_selectedSongName);
         return "redirect:/";
     }
 
-    //現状、selectedSongIndexが５以上になった際のエラー対処をしていない
     //selectedSongIndexは、ランダム処理する度に初期化される必要がある。
     @RequestMapping(path = "/play/next",method = RequestMethod.GET)
     String playNextSong(RedirectAttributes attributes){
         getNextSong();
         if(selectedSongIndex>=5)  //５つ目まで再生したら、初めの曲に戻る。
             selectedSongIndex=0;
-        
+
         String _selectedSongName=presentSongsList[selectedSongIndex].getFile_name();
         attributes.addFlashAttribute("selectedSong",_selectedSongName);
         return "redirect:/";
     }
 
 
-    //シャッフル対象の曲(Song)の配列をインスタンス(何の？)に設定
-    //再生する５曲というよりは、対象となる曲全体のことを指すはず。
-    //peakQueueとの兼ね合いはどうするのか
+    //TODO
+    //ランダム処理で選ばれる５個の曲を設定する
     //↓ここもうちょっと綺麗にかける
     //呼び出し側で、setSongs(repository.findAll().toArray(new Song[repository.findAll().size()]));
     public void setSongs(Song[] songs){  //引数には、「シャッフル対象の曲」(たぶんrepository.findAll())
         this.songsList.clear();
         Collections.addAll(this.songsList,songs);  //これ単体だと、呼び出されるたびにsongsListに(前の情報を残したまま)「追加」され続けてしまう。
     }
+
+    //TODO
+    //設定する値、あるいは設定先(presentSongsList)を見直す必要あり
+//    public void setSongs(Song[]songs){
+//        this.presentSongsList=songs;
+//    }
+
 
 
     //次に再生する曲(Song)を返す。次に返す曲が更新される。再生ボタン押されたとき(playSong()の中で？)呼ばれる。
